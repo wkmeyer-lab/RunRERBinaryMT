0f38d1f6754f8b2fe3a9a81ba3b69d67ab1812ef
MichealTene7 <110479175+MichealTene7@users.noreply.github.com>
2022-08-10 12:50:41 -0400

    add srcipt source to runMe

--- runMe.R
+++ runMe.R
@@ -1,2 +1,8 @@
+#Remove the pre-existing gitHUb remote
 system('git remote remove origin')
 
+#Load all source files in /reu and /reu/import
+file.sources = list.files(c("src/reu", "src/reu/import"), 
+                          pattern="*.R$", full.names=TRUE, 
+                          ignore.case=TRUE)
+sapply(file.sources,source,.GlobalEnv)
