c25d3a86aa71afcf6887ab036d65a0c9a576ccc4
MichealTene7 <110479175+MichaelTene7@users.noreply.github.com>
2022-10-06 16:09:23 -0400

    categorizePaths debug

--- Src/Loc/categorizationDemo.R
+++ Src/Loc/categorizationDemo.R
@@ -1,6 +1,7 @@
 library(RERconverge)
 source("Src/Reu/ZonomNameConvertMatrixCommon.R")
 source("Src/Reu/ZonomNameConvertVector.R")
+source("Src/Reu/categorizePaths.R")
 
 allInsectivoryData = read.csv("Results/allInsectivoryCorrelationFile.csv")
 
@@ -17,8 +18,15 @@ binaryTree = readRDS("Results/allInsectivoryBinaryForegroundTree.rds")
 plotTree(binaryTree)
 
 zonomMaster = readRDS("Data/RemadeTreesAllZoonomiaSpecies.rds")
-functionPaths = categorizePaths(binaryTree, zonomMaster, "functionPath", overwrite = T)
+functionPaths = categorizePaths(binaryTree, zonomMaster, "functionPath", overwrite = T) #rename this if you want to make a new one
+premadefunctionPaths = tree2Paths(readRDS("Results/premadefunctionPathManualFGTree.rds"), zonomMaster)
+plotRers(CNRers,"KIAA0825", Paths, sortrers = T)
 plotRers(CNRers,"KIAA0825", functionPaths, sortrers = T)
+plotRers(CNRers,"KIAA0825", premadefunctionPaths, sortrers = T)
 
-plotRers(CNRers,"ZNF292", manualPaths, sortrers = T)
+
 plotRers(CNRers,"ZNF292", Paths, sortrers = T)
+plotRers(CNRers,"ZNF292", functionPaths, sortrers = T)
+plotRers(CNRers,"ZNF292", premadefunctionPaths, sortrers = T)
+
+
