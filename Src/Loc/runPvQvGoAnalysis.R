a6b69d2a36d697c0554f2364c3e25f0082a06c97
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-03-07 16:55:18 -0500

    initial commit; calculates q values, displays p and q value heads and p value histograms

---
+++ Src/Loc/runPvQvGoAnalysis.R
@@ -0,0 +1,158 @@
+.libPaths("/share/ceph/wym219group/shared/libraries/R4") #add path to custom libraries to searched locations
+library(RERconverge)
+library("tools")
+source("Src/Reu/cmdArgImport.R")
+library(qvalue)
+library(insight)
+library(stringr)
+
+#---- USAGE -----
+#Used to generate various plots for data analysis. 
+#Makes: 
+  #P value table; Q value table; Histograms of p-values for positive Rho entires, histogram of p-values for negative Rho entires. 
+
+# ARUGMENTS: 
+#If an argument contains a '(' it is evaluated as code.
+# 'r="filePrefix"'                          This is the prefix attached to all files; a required argument. 
+# g = <T OR F>                              This sets if Gene Ontology analysis is run. Default TRUE. 
+# f = "permulationPvalueFileLocation.rds"   This is a manual override to specify the script use a specific Permulation p-value file. 
+  #If using any permulation p-value file other than "CombinedPrunedFastAll" with no run instance number, it must be specified manually.
+#testing args: 
+args = c('r=CVHRemake')
+{
+  #---- Initial Setup -----
+  #------------------------
+  
+  # --- Import prefix ----
+  args = commandArgs(trailingOnly = TRUE)
+  paste(args)
+  message(args)
+  
+  #File Prefix
+  if(!is.na(cmdArgImport('r'))){
+    filePrefix = cmdArgImport('r')
+  }else{
+    stop("THIS IS AN ISSUE MESSAGE; SPECIFY FILE PREFIX")
+  }
+  
+  #------------
+  
+  #------ Make Output directory -----
+  
+  #Make output directory if it does not exist
+  if(!dir.exists("Output")){
+    dir.create("Output")
+  }
+  #Make a specific subdirectory if it does not exist 
+  outputFolderNameNoSlash = paste("Output/",filePrefix, sep = "")
+  #create that directory if it does not exist
+  if(!dir.exists(outputFolderNameNoSlash)){
+    dir.create(outputFolderNameNoSlash)
+  }
+  outputFolderName = paste("Output/",filePrefix,"/", sep = "")
+  
+  #----------
+}
+
+{
+  #----Startup----
+  #----------------
+  
+  #--Default Values-- 
+  performGeneOntolgy = TRUE
+  usePermulationPValOverride = FALSE
+  permulationPValOverride = NULL 
+  metacombineValue = FALSE
+  
+  #--Import arguments-- 
+  
+  #Import if should perform gene ontology 
+  if(!is.na(cmdArgImport('g'))){
+    performGeneOntolgy = cmdArgImport('d')
+    performGeneOntolgy = as.logical(performGeneOntolgy)
+    if(is.na(performGeneOntolgy)){
+      performGeneOntolgy = TRUE
+      message("Perform gene ontology value not interpretable as logical. Did you remember to capitalize? Using TRUE.")
+    }
+  }else{
+    message("Perform gene ontology value not specified, using TRUE.")
+  }
+  
+  #Import permulation p-value Override 
+  if(!is.na(cmdArgImport('f'))){
+    usePermulationPValOverride = TRUE
+    permulationPValOverride = cmdArgImport('f')
+    message("Using Manually specified permulation p-value file. NOTE this setting ignores the meta-combination argument.")
+  }else{
+    message("No Permulation pValue override, using standard CombinedPrunedFastAllPermulationsPValue.rds.")
+  } 
+  
+  #--
+}
+
+#---- MAIN CODE ----
+#-------------------
+
+# --- Import the data --- 
+correlationFileLocation = paste(outputFolderName, filePrefix, "CorrelationFile.rds", sep= "")
+
+if(usePermulationPValOverride){
+  permulationFileLocation = permulationPValOverride
+}else{
+  permulationFileLocation = paste(outputFolderName, filePrefix, "CombinedPrunedFastAllPermulationsPValue.rds", sep= "")
+}
+
+correlData = readRDS(correlationFileLocation)                            #Import the correlation data (non-permulated)
+correlData$permPValue = readRDS(permulationFileLocation)                 #Add a collumn to the data with the permulation p Values
+
+#Make Q values 
+qValueObject = qvalue(p = correlData$permPValue)
+correlData$qValue = qValueObject$qvalues
+
+#Make separate objects for positive and negative Rho 
+correlDataPositive = correlData[which(correlData$Rho > 0),]
+correlDataNegative = correlData[which(correlData$Rho < 0),]
+correlDataZero = correlData[which(correlData$Rho = 0),]
+
+
+
+
+# --- Display the data ---
+outputPDFLocation = paste(outputFolderName, filePrefix, "Graphs-PvalQvalGo.pdf", sep= "")
+
+#function to make text plots
+makeTextPlot = function(data, collumn){
+  ValueHead = head(data[order(collumn),], n=40)
+  ValueHead = round(ValueHead, digits = 7) 
+  ValueHead = format_table(ValueHead, pretty_names = F, digits = 7)
+  ValueHead$N = str_sub(ValueHead$N, end = -9)
+  ValueHead
+}
+
+
+#Plot the top genes
+if(performGeneOntolgy){
+  pdf(file = outputPDFLocation, width = 15, height = 30)
+}else{
+  pdf(file = outputPDFLocation, width = 15, height = 15)
+}
+par(mfrow = c(2,2))
+pValueHead = makeTextPlot(correlData, correlData$permPValue)
+textplot(pValueHead, mar = c(0,0,2,0), cmar = 1.5)
+title(main = "Top genes by p-Value")
+qValueHead = makeTextPlot(correlData, correlData$qValue)
+textplot(qValueHead, mar = c(0,0,2,0), cmar = 1.5)
+title(main = "Top genes by q-Value")
+
+#plot p value histograms 
+hist(correlDataPositive$permPValue, breaks = 40, xaxp = c(0,1,20))
+hist(correlDataNegative$permPValue, breaks = 40, xaxp = c(0,1,20))
+
+if(performGeneOntolgy){
+  #Plot the GO Outputs now 
+}
+dev.off()
+
+
+
+
