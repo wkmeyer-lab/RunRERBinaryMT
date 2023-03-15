70aedfbaa7c92ba58f5d512a6116cfd1af234f43
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-03-15 18:19:47 -0400

    initial commit of showRERPlots reusable function

---
+++ Src/Reu/showRERPlots.R
@@ -0,0 +1,19 @@
+library(RERconverge)
+#Usage:
+#Shows the RER plots of the genes with the top correlations, ticking through one every four seconds. 
+
+showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start = 1){
+  if(usePerm){
+    message('Expects the collumn containing the permulation P values to be named "permPVal"')
+    genesRankedPermP = rownames(CorrelationFile[order(CorrelationFile$permPVal),])
+  }else{
+    genesRankedPermP = rownames(CorrelationFile[order(CorrelationFile$p.adj),])
+  }
+  
+  for(i in start:100){
+    plotRers(domesticationRERs, genesRankedPermP[i], domesticationPaths)
+    message(i)
+    Sys.sleep(4)
+  }
+  
+}
