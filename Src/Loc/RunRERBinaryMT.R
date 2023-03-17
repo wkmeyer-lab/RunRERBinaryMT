96b9074e727579555482af55851270baf5c02fa6
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-03-17 17:42:04 -0400

    edit debug args

--- Src/Loc/RunRERBinaryMT.R
+++ Src/Loc/RunRERBinaryMT.R
@@ -25,6 +25,7 @@ source("Src/Reu/cmdArgImport.R")
 #Test args: 
 args = c('m=data/RemadeTreesAllZoonomiaSpecies.rds', 'r=demoInsectivory')
 args = c('m=Data/FishTree.rds', 'r=carnvHerbsOldRebuild', 'v=F')
+args = c('m=Data/RemadeTreesAllZoonomiaSpecies.rds', 'r=Domestication')
 
 # ---- Default values if no arguments
 
