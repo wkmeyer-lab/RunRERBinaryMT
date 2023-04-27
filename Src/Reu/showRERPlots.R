e3bcdd1062b5a8e5d3aec53521a63bc2a3bf119c
Michael Tene <110479175+MichaelTene7@users.noreply.github.com>
2023-04-27 13:55:02 -0400

    add function to sort plotted RERs

--- Src/Reu/showRERPlots.R
+++ Src/Reu/showRERPlots.R
@@ -2,7 +2,7 @@ library(RERconverge)
 #Usage:
 #Shows the RER plots of the genes with the top correlations, ticking through one every four seconds. 
 
-showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start = 1){
+showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start = 1, ranked = F){
   if(usePerm){
     message('Expects the collumn containing the permulation P values to be named "permPVal"')
     genesRankedPermP = rownames(CorrelationFile[order(CorrelationFile$permPVal),])
@@ -11,9 +11,10 @@ showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start 
   }
   
   for(i in start:100){
-    plotRers(RERFile, genesRankedPermP[i], PathsFile)
+    plotRers(RERFile, genesRankedPermP[i], PathsFile, sortrers = ranked)
     message(i)
     Sys.sleep(4)
   }
   
 }
+
