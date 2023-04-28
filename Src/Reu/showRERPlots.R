ee3965418ace0c4a035ea5ebd5aa3aa2ece8d07c
Michael Tene <110479175+MichaelTene7@users.noreply.github.com>
2023-04-28 16:36:55 -0400

    add costumizable end points and sleep time

--- Src/Reu/showRERPlots.R
+++ Src/Reu/showRERPlots.R
@@ -2,7 +2,7 @@ library(RERconverge)
 #Usage:
 #Shows the RER plots of the genes with the top correlations, ticking through one every four seconds. 
 
-showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start = 1, sort = F){
+showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start = 1, sort = F, end = 15, sleep = 4){
   if(usePerm){
     message('Expects the collumn containing the permulation P values to be named "permPVal"')
     genesRankedPermP = rownames(CorrelationFile[order(CorrelationFile$permPVal),])
@@ -10,10 +10,10 @@ showRERPlots = function(RERFile, PathsFile, CorrelationFile, usePerm = F, start 
     genesRankedPermP = rownames(CorrelationFile[order(CorrelationFile$p.adj),])
   }
   
-  for(i in start:100){
+  for(i in start:end){
     plotRers(RERFile, genesRankedPermP[i], PathsFile, sortrers = sort)
     message(i)
-    Sys.sleep(4)
+    Sys.sleep(sleep)
   }
   
 }
