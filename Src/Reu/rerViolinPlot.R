fca4961d58292ca306ac1a41ab3088931007167c
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-04-28 01:36:03 -0400

    Add rerViolinPlot function

---
+++ Src/Reu/rerViolinPlot.R
@@ -0,0 +1,32 @@
+rerViolinPlot = function(mainTrees, RERObject, phenotypeTree, foregroundSpecies, geneOfInterest, foregroundName = "Foreground", BackgroundName = "Background"){
+  source("Src/Reu/RERConvergeFunctions.R")
+  rerTree = returnRersAsTree(mainTrees, RERObject, geneOfInterest, foregroundSpecies, plot = F)
+  relativeRate = rerTree$edge.length
+  geneTree = mainTrees$trees[[geneOfInterest]]
+  phenotypePath = tree2Paths(phenotypeTree, mainTrees)
+  
+  edgeIndexMap = edgeIndexRelativeMaster(geneTree, mainTrees$masterTree)
+  relevantBranches = mainTrees$matIndex[edgeIndexMap[,c(2,1)]]
+  relevantPath = phenotypePath[relevantBranches]
+  
+  phenotypeVector = c(rep(NA, length(relativeRate)))
+  phenotypeVector[relevantPath == 1] = foregroundName
+  phenotypeVector[relevantPath == 0] = BackgroundName
+  
+  #Adding Variables with different caps for better Aesthetics 
+  Phenotype = phenotypeVector
+  RelativeRate = relativeRate
+  
+  rateWithPhenotype = data.frame(RelativeRate, Phenotype)
+  rateWithPhenotype = rateWithPhenotype[!is.na(rateWithPhenotype$Phenotype),]
+  
+  plot = ggplot(rateWithPhenotype, aes(x=Phenotype, y=RelativeRate, col=Phenotype)) + 
+    geom_violin(adjust=1/3) +
+    geom_jitter(position=position_jitter(0.2)) +
+    theme_classic() +
+    scale_color_manual(values=c("lightsalmon","darkgreen")) +
+    theme(text = element_text(size = 20))+
+    ggtitle(geneOfInterest)
+  
+  return(plot)
+}
