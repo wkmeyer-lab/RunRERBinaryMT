dd1545e204520fb4462f9d154f83f62d40851971
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-05-17 15:04:32 -0400

    Add a formatting for a standardized script header

---
+++ Src/Reu/standardScriptHeader.R
@@ -0,0 +1,62 @@
+# -- Libraries 
+.libPaths("/share/ceph/wym219group/shared/libraries/R4") #add path to custom libraries to searched locations
+library(RERconverge)
+source("Src/Reu/cmdArgImport.R")
+
+# -- Usage:
+# This text describes the purpose of the script 
+
+# -- Command arguments list
+
+#----------------
+# --- Standard start-up code ---
+args = commandArgs(trailingOnly = TRUE)
+{  # Bracket used for collapsing purposes
+  source("Src/Reu/cmdArgImport.R")
+  #File Prefix
+  if(!is.na(cmdArgImport('r'))){
+    filePrefix = cmdArgImport('r')
+  }else{
+    stop("THIS IS AN ISSUE MESSAGE; SPECIFY FILE PREFIX")
+  }
+  
+  #  Output Directory 
+  if(!dir.exists("Output")){                                      #Make output directory if it does not exist
+    dir.create("Output")
+  }
+  outputFolderNameNoSlash = paste("Output/",filePrefix, sep = "") #Set the prefix sub directory
+  if(!dir.exists(outputFolderNameNoSlash)){                       #create that directory if it does not exist
+    dir.create(outputFolderNameNoSlash)
+  }
+  outputFolderName = paste("Output/",filePrefix,"/", sep = "")
+  
+  #  Force update argument
+  forceUpdate = FALSE
+  if(!is.na(cmdArgImport('v'))){                                 #Import if update being forced with argument 
+    forceUpdate = cmdArgImport('v')
+    forceUpdate = as.logical(forceUpdate)
+  }else{
+    paste("Force update not specified, not forcing update")
+  }
+}
+
+# --- Argument Imports ---
+# Defaults
+argument1Holder = TRUE
+
+{ # Bracket used for collapsing purposes
+  #Import first argument
+  if(!is.na(cmdArgImport('X'))){
+    argument1Holder = cmdArgImport('X')
+    argumentHolder = as.logical(argument1Holder)
+    if(is.na(argument1Holder)){
+      argument1Holder = TRUE
+      message("Argument value not interpretable as logical. Did you remember to capitalize? Using TRUE.")
+    }
+  }else{
+    message("Argument value not specified, using TRUE.")
+  }
+}
+
+
+# -- Code Body --- 
