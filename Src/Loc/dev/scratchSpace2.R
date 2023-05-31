715514d791031e96a18e3f57ba93a46fcf4fb57f
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-05-31 18:28:45 -0400

    update

--- Src/Loc/dev/scratchSpace2.R
+++ Src/Loc/dev/scratchSpace2.R
@@ -4,10 +4,36 @@ permdata = readRDS("Output/CategoricalDiet3Phen/CategoricalDiet3PhenPermulations
 
 permdtaaTrim = permdata
 
-permdtaaTrim[[2]] = permdtaaTrim[[2]][1:10]
-permdtaaTrim[[3]] = permdtaaTrim[[3]][1:10]
+permdtaaTrim[[2]] = permdtaaTrim[[2]][1:3]
+permdtaaTrim[[3]] = permdtaaTrim[[3]][1:3]
 
 
 saveRDS(permdtaaTrim, "Output/CategoricalDiet3Phen/CategoricalDiet3PhenPermulationsDataDev.rds")
 rm(permdata)
 rm(permdtaaTrim)
+
+library(data.table)
+source("Src/Reu/CategoricalPermulationsParallelFunctions.R")
+
+permCorrelations = getPermPvalsCategorical3(correlationsObject, permulationsData$trees, phenotypeVector, mainTrees, RERObject)
+permCorrelations = getPermPvalsCategorical2(correlationsObject, permulationsData$trees, phenotypeVector, mainTrees, RERObject)
+
+permCorrelations = CategoricalPermulationGetCor(correlationsObject, permulationsData$trees, phenotypeVector, mainTrees, RERObject, report=T)
+
+time1 = Sys.time()
+time2 = Sys.time()
+timeDif = time2 - time1
+str(timeDif)
+attr(timeDif, "units")
+
+
+permPval = CategoricalCalculatePermulationPValues(correlationsObject, permCorrelations)
+
+?correlateWithCategoricalPhenotype
+
+all.equal(paths, pathsObject)
+modelType ="ER"
+
+#categoricalCorrelationOld = categoricalCorrelation
+
+all.equal(categoricalCorrelation[[2]][1], categoricalCorrelationOld[[2]][1])
