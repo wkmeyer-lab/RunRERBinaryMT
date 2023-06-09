0443cb164877db06dbdea43dc02462036e006a1e
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-06-09 19:25:32 -0400

    Add functionality for background coloring

--- Src/Reu/ZoonomTreeNameToCommon.R
+++ Src/Reu/ZoonomTreeNameToCommon.R
@@ -10,9 +10,40 @@
 #treeToConvertLocation = "Data/CVHRemakeBinaryForegroundTree.rds"
 #inputTree = readRDS(treeToConvertLocation)
 
+ZoonomTreeNameToCommon = function(tree, plot = T, isForegroundTree = T, manualAnnotLocation = "Data/manualAnnotationsSheet.csv", hlcol = "blue", bgcol = "black"){
+  library(RERconverge)
+  
+  manualAnnot = read.csv(manualAnnotLocation) 
+  inputTree = tree
+  tipNames = inputTree$tip.label
+  for(i in 1:length(tipNames)){                                                    #for each name: 
+    currentName = tipNames[i]                                                      #use the 'i'th name in the list
+    currentRow = manualAnnot[manualAnnot$FaName %in% currentName, ]             #find a row with the zonom name that matches the current name 
+    currentSize = dim(currentRow)                                               #Part of "does row exist check": get the dimensions of the currentRow dataframe
+    obsNumber = currentSize[1]                                                  #set "size" equal to the number of observations in 'currentRow'; which is the number of matches to the current name. If none exist it will be 0, if more than one it will be greater than 1. 
+    if(obsNumber == 1){                                                         #if only one match exists:
+      currentName = currentRow$Common.Name.or.Group                             #get the name from that row
+    }else{
+      currentName = tipNames[i]                                                    #Otherwise, keep the name the same
+    }
+    tipNames[i] = currentName                                                      #update the main name list with the name chose
+  }
+  inputTree$tip.label = tipNames
+  
+  if(plot){
+    if(isForegroundTree){
+      readableTree = inputTree
+      readableTree$edge.length[readableTree$edge.length == 0] = 1
+      plotTreeHighlightBranches2(readableTree, hlspecies = which(inputTree$edge.length == 1), hlcols = hlcol, bgcol = bgcol)
+    }else{
+      plotTree(inputTree)
+    }
+  }
+  return(inputTree)
+}
 
 plotTreeHighlightBranches2 =function (tree, outgroup = NULL, hlspecies, hlcols = NULL, main = "", 
-          useGG = FALSE, bgcols = "black") 
+          useGG = FALSE, bgcol = "black") 
 {
   if (is.null(hlcols)) {
     hlcols <- c(2:(length(hlspecies) + 1))
@@ -115,34 +146,4 @@ plotTreeHighlightBranches2 =function (tree, outgroup = NULL, hlspecies, hlcols =
   }
 }
 
-ZoonomTreeNameToCommon = function(tree, plot = T, isForegroundTree = T, manualAnnotLocation = "Data/manualAnnotationsSheet.csv", hlcol = "blue", bgcol = "black"){
-  library(RERconverge)
-  
-  manualAnnot = read.csv(manualAnnotLocation) 
-  inputTree = tree
-  tipNames = inputTree$tip.label
-  for(i in 1:length(tipNames)){                                                    #for each name: 
-    currentName = tipNames[i]                                                      #use the 'i'th name in the list
-    currentRow = manualAnnot[manualAnnot$FaName %in% currentName, ]             #find a row with the zonom name that matches the current name 
-    currentSize = dim(currentRow)                                               #Part of "does row exist check": get the dimensions of the currentRow dataframe
-    obsNumber = currentSize[1]                                                  #set "size" equal to the number of observations in 'currentRow'; which is the number of matches to the current name. If none exist it will be 0, if more than one it will be greater than 1. 
-    if(obsNumber == 1){                                                         #if only one match exists:
-      currentName = currentRow$Common.Name.or.Group                             #get the name from that row
-    }else{
-      currentName = tipNames[i]                                                    #Otherwise, keep the name the same
-    }
-    tipNames[i] = currentName                                                      #update the main name list with the name chose
-  }
-  inputTree$tip.label = tipNames
-  
-  if(plot){
-    if(isForegroundTree){
-      readableTree = inputTree
-      readableTree$edge.length[readableTree$edge.length == 0] = 1
-      plotTreeHighlightBranches2(readableTree, hlspecies = which(inputTree$edge.length == 1), hlcols = hlcol, bgcol = bgcol)
-    }else{
-      plotTree(inputTree)
-    }
-  }
-  return(inputTree)
-}
+
--- Src/Reu/makeMasterAndGeneTreePlots.R
+++ Src/Reu/makeMasterAndGeneTreePlots.R
@@ -1,4 +1,7 @@
-makeMasterVsGeneTreePlots = function(mainTrees, RERObject, geneInQuestion, foregroundVector, colors = "blue", correlationPlot = F){
+
+
+
+makeMasterVsGeneTreePlots = function(mainTrees, RERObject, geneInQuestion, foregroundVector, fgcols = "blue", correlationPlot = F, bgcolor = "black", rmlabels = NULL){
   source("Src/Reu/ZonomNameConvertVector.R")
   source("Src/Reu/ZoonomTreeNameToCommon.R")
   source("Src/Reu/GetForegroundEdges.R")
@@ -18,13 +21,18 @@ makeMasterVsGeneTreePlots = function(mainTrees, RERObject, geneInQuestion, foreg
   
   par(mfrow = c(1,2), mai = c(0.5, 0.1, 0.2, 0.1))
   masterFGEdges = getForegroundEdges(commonMaster, foregroundVector)
-  plotTreeHighlightBranches(commonMaster, hlspecies = masterFGEdges, main = "Overall Genome Average", hlcols = colors)
+  plotTreeHighlightBranches2(commonMaster, hlspecies = masterFGEdges, main = "Overall Genome Average", hlcols = fgcols, bgcol = bgcolor)
   
   geneFGEdges = getForegroundEdges(commonGene, foregroundVector)
-  plotTreeHighlightBranches(commonGene, main = paste("Gene:", geneInQuestion), hlspecies = geneFGEdges, hlcols = colors)
+  plotTreeHighlightBranches2(commonGene, main = paste("Gene:", geneInQuestion), hlspecies = geneFGEdges, hlcols = fgcols, bgcol = bgcolor)
   
   if(correlationPlot){
     if(all.equal(masterFGEdges, geneFGEdges)){ #Only run this code if the trees are the same shape
+      
+      if(!is.null(rmlabels)){
+        commonMaster$tip.label[which(commonMaster$tip.label %in% rmlabels)] = "   "
+        commonGene$tip.label[which(commonGene$tip.label %in% rmlabels)] = "   "
+      }
       
       edgeGround = rep(NA, length(commonMaster$edge.length))
       edgeGround[masterFGEdges] = 1
@@ -44,10 +52,10 @@ makeMasterVsGeneTreePlots = function(mainTrees, RERObject, geneInQuestion, foreg
                   ) + 
                   scale_size_manual(values = c(1, 1, 1, 1)) + 
                   geom_point(aes(size = Ground)) + 
-                  scale_color_manual(values = c("black", colors)) + 
+                  scale_color_manual(values = c(bgcolor, fgcols)) + 
                   coord_fixed() + 
                   expand_limits(x = max(edgeCorrealtions$Gene), y = max(edgeCorrealtions$Master))+
-                  geom_text(hjust = "bottom", size = 4, check_overlap = T) + 
+                  geom_text(hjust = "bottom", size = 4, check_overlap = T, hjust = 1) + 
                   ylab("Gene-Specific Branch Length") + 
                   xlab("Genome Average Branch Length") + 
                   #ggtitle(plottitle) + 
@@ -70,3 +78,107 @@ makeMasterVsGeneTreePlots = function(mainTrees, RERObject, geneInQuestion, foreg
   
 }
 
+
+plotTreeHighlightBranches2 =function (tree, outgroup = NULL, hlspecies, hlcols = NULL, main = "", 
+                                      useGG = FALSE, bgcol = "black") 
+{
+  if (is.null(hlcols)) {
+    hlcols <- c(2:(length(hlspecies) + 1))
+  }
+  if (length(hlcols) < length(hlspecies)) {
+    hlcols <- rep_len(hlcols, length(hlspecies))
+  }
+  if (!is.null(outgroup)) {
+    outgroup <- outgroup[outgroup %in% tree$tip.label]
+    if (length(outgroup) > 0) {
+      if (is.numeric(hlspecies)) {
+        dummytree <- tree
+        dummytree$edge.length <- c(rep(1, nrow(dummytree$edge)))
+        for (i in c(1:length(hlspecies))) {
+          dummytree$edge.length[hlspecies] <- i + 1
+        }
+        dummyrooted <- root(dummytree, outgroup)
+      }
+      rooted <- root(tree, outgroup)
+    }
+    else {
+      print("No members of requested outgroup found in tree; keeping unrooted.")
+      rooted <- tree
+      outgroup <- NULL
+    }
+  }
+  else {
+    rooted <- tree
+  }
+  if (useGG) {
+    if (is.numeric(hlcols)) {
+      hlcols <- rep_len("#0000ff", length(hlspecies))
+    }
+    hlspecies_named <- vector(mode = "character")
+    if (is.numeric(hlspecies)) {
+      for (i in 1:length(hlspecies)) {
+        hlspecies_named[i] <- tree$tip.label[hlspecies[i]]
+      }
+    }
+    else hlspecies_named <- hlspecies
+    rooted2 <- rooted
+    mm <- min(rooted2$edge.length[rooted2$edge.length > 0])
+    rooted2$edge.length[rooted2$edge.length == 0] <- max(0.02, 
+                                                         mm/20)
+    tipCols <- vector(mode = "character")
+    x <- 1
+    for (i in 1:length(tree$tip.label)) {
+      if (tree$tip.label[i] %in% hlspecies_named) {
+        tipCols[i] <- hlcols[x]
+        x <- x + 1
+      }
+      else tipCols[i] <- bgcol
+    }
+    nlabel <- rooted2$Nnode + length(rooted2$tip.label)
+    edgeCols <- vector(mode = "character", length = nlabel)
+    x <- 1
+    for (i in 1:nlabel) {
+      if (i < length(rooted2$tip.label)) {
+        if (rooted2$tip.label[i] %in% hlspecies_named) {
+          edgeCols[i] <- hlcols[x]
+          x <- x + 1
+        }
+        else edgeCols[i] <- bgcol
+      }
+      else edgeCols[i] <- bgcol
+    }
+    plotobj = ggtree(rooted2, color = edgeCols)
+    plotobj = plotobj + geom_tiplab(color = tipCols, geom = "text", 
+                                    cex = 3) + labs(title = main)
+    return(plotobj)
+  }
+  else {
+    colMaster <- c(rep(bgcol, nrow(rooted$edge)))
+    if (is.numeric(hlspecies)) {
+      if (!is.null(outgroup)) {
+        hlcols <- c(bgcol, hlcols)
+        colMaster <- hlcols[dummyrooted$edge.length]
+      }
+      else {
+        for (i in 1:length(hlspecies)) {
+          colMaster[hlspecies[i]] <- hlcols[i]
+        }
+      }
+    }
+    else {
+      wspmr <- rooted$tip.label[rooted$edge[, 2]]
+      for (i in 1:length(hlspecies)) {
+        colMaster[which(wspmr == hlspecies[i])] <- hlcols[i]
+      }
+    }
+    termedge <- order(rooted$edge[, 2])[1:length(rooted$tip.label)]
+    colMasterTip <- colMaster[termedge]
+    rooted2 = rooted
+    mm = min(rooted2$edge.length[rooted2$edge.length > 0])
+    rooted2$edge.length[rooted2$edge.length == 0] = max(0.02, 
+                                                        mm/20)
+    plotobj = plot.phylo(rooted2, main = main, edge.color = colMaster, 
+                         tip.color = colMasterTip, edge.width = 2, cex = 0.8)
+    return(plotobj)
+  }
+}
