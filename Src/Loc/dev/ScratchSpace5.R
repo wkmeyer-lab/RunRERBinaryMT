4f45bd0cbf0254a055cc6d3e3718057d5c69f4ef
Michael Tene <110479175+MichaelTene7@users.noreply.github.com>
2025-06-17 14:56:22 -0400

    scratch update

--- Src/Loc/dev/ScratchSpace5.R
+++ Src/Loc/dev/ScratchSpace5.R
@@ -4,14 +4,128 @@ a = b #prevent full runs
 # ------------------------------------------------------------------
 # --- Work on making a tree figure for lalitha  ----- 
 # ------------------------------------------------------------------
-lalithaData = read.table("C:/Users/mit221/Downloads/fixed_Supermatrix.tsv")
-laltihaSpecies = lalithaData[1,]
+lalithaData = read.csv("C:/Users/mit221/Downloads/SpeciesNamesAndPhenosForDE72.csv")
 
-laltihaSpecies = unlist(laltihaSpecies)
-laltihaSpecies = laltihaSpecies[-1]
-laltihaSpecies = gsub("[0-9]+$", "", laltihaSpecies)
-laltihaSpecies = unique(laltihaSpecies)
 
+laltihaSpecies = lalithaData[,1]
+
+#laltihaSpecies = unlist(laltihaSpecies)
+#laltihaSpecies = laltihaSpecies[-1]
+#laltihaSpecies = gsub("[0-9]+$", "", laltihaSpecies)
+#laltihaSpecies = unique(laltihaSpecies)
+
+mergeData = read.csv("Data/mergedData.csv")
+
+for(i in 1:length(laltihaSpecies)){
+laltihaSpecies[i] = gsub(" ", "_", laltihaSpecies[i]) #replace spaces with underscores 
+laltihaSpecies[i] = sub('^([^_]+_[^_]+).*', '\\1', laltihaSpecies[i]) #remove anything  after a second underscore
+laltihaSpecies[i] = tolower(laltihaSpecies[i])
+}
+
+lalithaData[,1] = laltihaSpecies
+write.csv(lalithaData, "C:/Users/mit221/Downloads/SpeciesNamesAndPhenosForDE72.csv")
+
+sum(!laltihaSpecies %in% mergeData$Scientific_Binomial)
+
+
+ggTreeOut = ggtree(commonCategoricalTree) +scale_color_manual(values=palette()) 
+
+
+# ------------------------------------------------------------------
+# --- Making a make basic radial tree script  ----- 
+# ------------------------------------------------------------------
+
+
+commonMainTrees = mainTrees
+commonMainTrees$masterTree = ZoonomTreeNameToCommon(commonMainTrees$masterTree, manualAnnotLocation = spreadSheetLocation, tipCol = nameColumn)
+commonMasterTree = commonMainTrees$masterTree
+
+
+categoricalCommonTreeFilename = paste(outputFolderName, filePrefix, "CategoricalCommonTree.rds", sep="") #make a filename based on the prefix
+commonCategoricalTree = readRDS(categoricalCommonTreeFilename)
+scientificCategoricalTreeFilename = paste(outputFolderName, filePrefix, "CategoricalScientificTree.rds", sep="") #make a filename based on the prefix
+scientificCategoricalTree = readRDS(scientificCategoricalTreeFilename)
+
+
+commonMasterTrimmed = drop.tip(commonMasterTree, commonMasterTree$tip.label[!commonMasterTree$tip.label %in% commonCategoricalTree$tip.label])
+scientificMasterTrimmed = ZoonomTreeNameToCommon(commonMasterTrimmed, manualAnnotLocation = spreadSheetLocation, tipCol = "CommonName", scientific = T, scientificCol = "Scientific_Binomial")
+
+commonCategoricalTreeEdgeLengths = commonCategoricalTree$edge.length
+commonCategoricalTreeEdgeLengths = as.character(commonCategoricalTreeEdgeLengths)
+edge=data.frame(commonCategoricalTree$edge, edge_num=1:nrow(commonCategoricalTree$edge))
+colnames(edge)=c("parent", "node", "edge_num")
+edge$Categorylength = commonCategoricalTree$edge.length
+edge$CategorylengthChar = as.character(edge$Categorylength)
+if(!is.null(CategoryReplacements)){
+  for(i in 1:length(unique(edge$CategorylengthChar))){
+    edge$CategorylengthChar[edge$CategorylengthChar == i] = CategoryReplacements[i]
+  }
+}
+
+commonCategoricalTree$edge.length = commonMasterTrimmed$edge.length
+scientificCategoricalTree$edge.length = scientificMasterTrimmed$edge.length
+
+
+
+
+phylopicNames = NULL
+uuidList = NULL
+missingPictures = NA
+uuidListFilename =  paste(outputFolderName, filePrefix, "UuidList.rds", sep="") #make a filename based on the prefix
+if(!file.exists(uuidListFilename) | forceUpdate){                             #if it does not exist, or update is forced 
+  inTips = scientificCategoricalTree$tip.label
+  for(i in 1:length(inTips)){
+    print(i)
+    phylopicNames[i] = tryCatch({autocomplete_name(inTips[i])[1,2]}, error = function(msg) {return(inTips[i])})
+    
+    uuidList[i] = tryCatch(
+      {get_uuid(phylopicNames[i])}, 
+      error = function(msg){
+        if(length(grep(" ", phylopicNames[i])) > 0){genusName = strsplit(phylopicNames[i], " ")[[1]][1]}else{
+          if(length(grep("_", phylopicNames[i])) > 0){genusName = strsplit(phylopicNames[i], "_")[[1]][1]}
+        }
+        tryCatch(
+          {get_uuid(genusName)},
+          error = function(msg){
+            message(paste("No pic found for ", phylopicNames[i]))
+            missingReport = phylopicNames[i]
+            names(missingReport) = i 
+            append(missingPictures, missingReport)
+            return("NULL")
+          }
+        )
+      }
+    )
+  }
+  saveRDS(uuidList, uuidListFilename)
+}else{                                                                          #Otherwise
+  uuidList = readRDS(uuidListFilename)                                              #Use the existing ones
+}
+
+
+# Replace any missing UUIDs with tardigrades as debug images 
+uuidList[uuidList == "NULL"] = get_uuid("tardigrades")
+uuidList[21] = get_uuid("tardigrades")
+
+#uuidList[uuidList == "NULL"] = NULL
+
+tip_data = data.frame(
+  scientificlabel = scientificCategoricalTree$tip.label,
+  uuid = uuidList,
+  node = 1:length(scientificCategoricalTree$tip.label),
+  stringsAsFactors = FALSE
+)
+tip_data$uuid[tip_data$uuid == "NULL"] = NULL
+
+
+ggTreeOut = ggtree(commonCategoricalTree, layout = "circular") +scale_color_manual(values=palette()) 
+ggTreeOut = ggTreeOut %<+% edge + aes(color=CategorylengthChar)
+ggTreeOut = ggTreeOut %<+% tip_data 
+ggTreeOut$data$label = paste(ggTreeOut$data$label, "-", ggTreeOut$data$node, sep="")
+ggTreeOut = ggTreeOut + geom_tiplab()
+ggTreeOut = ggTreeOut + geom_tiplab(geom = "phylopic", aes(image = uuid))
+#ggTreeOut + geom_phylopic(aes(uuid = uuid), color = "black", alpha = 1, size = 0.08)
+ggTreeOut
 
 
 # ------------------------------------------------------------------
